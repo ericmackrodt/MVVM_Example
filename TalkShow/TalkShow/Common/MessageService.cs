d56b1b6d4c03d716c1492a039f683e7b4b4e4e57
Eric Mackrodt <ericmackrodt@gmail.com>
2014-04-10 02:35:30 -0300

    Added files

---
+++ TalkShow/TalkShow/Common/MessageService.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace TalkShow.Common
+{
+    public class MessageService : IMessageService
+    {
+        public void Show(string msg)
+        {
+            MessageBox.Show(msg);
+        }
+    }
+}
---
+++ TalkShow/TalkShow/ViewModels/MainViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
+using TalkShow.Common;
+using TalkShow.Models;
+
+namespace TalkShow.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private IMessageService msg;
+
+        private string input;
+        public string Input
+        {
+            get { return input; }
+            set
+            {
+                input = value;
+                NotifyChange("Input");
+            }
+        }
+
+        private ObservableCollection<Item> items;
+        public ObservableCollection<Item> Items
+        {
+            get { return items; }
+            set
+            {
+                items = value;
+                NotifyChange("Items");
+            }
+        }
+
+        private ICommand addCommand;
+        public ICommand AddCommand
+        {
+            get { return addCommand; }
+        }
+
+        public MainViewModel(IMessageService msgSrvc)
+        {
+            msg = msgSrvc;
+            addCommand = new RelayCommand(Add) { IsEnabled = true };
+        }
+
+        private void Add()
+        {
+            if (Items == null)
+                Items = new ObservableCollection<Item>();
+
+            Items.Add(new Item() { Name = Input });
+
+            msg.Show("Vai ser adicionado depois que apertar ok motherfucker!");
+        }
+
+        private void NotifyChange(string property)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}

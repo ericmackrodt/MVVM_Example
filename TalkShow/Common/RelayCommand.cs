1f0b3829e434073e3299e58185183120bedff7da
Eric Mackrodt <ericmackrodt@gmail.com>
2014-04-17 14:02:37 -0300

    Added Files

---
+++ TalkShow/Common/RelayCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace TalkShow.Common
+{
+    public class RelayCommand : ICommand
+    {
+        private bool isEnabled;
+        private Action action;
+
+        public bool IsEnabled
+        {
+            get { return isEnabled; }
+            set
+            {
+                isEnabled = value;
+                if (CanExecuteChanged != null)
+                    CanExecuteChanged(this, new EventArgs());
+            }
+        }
+
+        public RelayCommand(Action action)
+        {
+            this.action = action;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return IsEnabled;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            action();
+        }
+    }
+}
---
+++ TalkShow/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,27 @@
+﻿using Autofac;
+using TalkShow.Common;
+
+namespace TalkShow.ViewModels
+{
+    public class ViewModelLocator
+    {
+        private readonly IContainer container;
+
+        public ViewModelLocator()
+        {
+            var containerBuilder = new ContainerBuilder();
+            containerBuilder.RegisterType<MessageService>().As<IMessageService>();
+            containerBuilder.RegisterType<MainViewModel>();
+
+            this.container = containerBuilder.Build();
+        }
+
+        public MainViewModel MainViewModel
+        {
+            get
+            {
+                return this.container.Resolve<MainViewModel>();
+            }
+        }
+    }
+}
